9f1e996f64b5cada2135b2635d8c9c44614e1564
Aaron Thomas <aaron.thomas231@yahoo.comgit>
2023-01-05 20:27:26 -0800

    Finished

---
+++ src/GitHubCard/GitHubCard.js
@@ -0,0 +1,25 @@
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+
+function GitHubCard() {
+  return (
+    <div className="card">
+      <Card style={{ width: "30rem" }}>
+        <Card.Img
+          variant="top"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMNWvjIrRrWuNFRKRse29rnGanWWwqrISo8A&usqp=CAU"
+        />
+        <Card.Body>
+          <Card.Title>AaronThomas24</Card.Title>
+          <Card.Text>
+            Hello my name is Aaron Thomas. I am currently studying software
+            devolopment at Csu Long Beach.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+}
+
+export default GitHubCard;
